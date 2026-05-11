8c23e7daa87e37fbfb91351d40ca79a78b696b75
Miguel Angel Ferreira Moreno <miguel.fermo1985@gmail.com>
2026-05-10 22:06:25 -0600

    fix deploy issue

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,5 @@
 export const environment = {
     production: true,
     apiUrl: 'https://fortalezaspringapi.onrender.com/api/v1',
-    appVersion: '1.3.0',
-    envName: 'PROD'
+    appVersion: '1.3.0'
 }
